b66fa9a819de14edef6ac1ae9649096aeb777898
JosiahFu <josiahfu@gmail.com>
2024-02-14 15:40:51 -0800

    Remove dependency on fabric API

--- src/main/java/archives/tater/logicalanvil/mixin/AnvilScreenMixin.java
+++ src/main/java/archives/tater/logicalanvil/mixin/AnvilScreenMixin.java
@@ -2,12 +2,9 @@ package archives.tater.logicalanvil.mixin;
 
 import archives.tater.logicalanvil.LogicalAnvil;
 import net.minecraft.client.gui.screen.ingame.AnvilScreen;
-import net.minecraft.entity.player.PlayerAbilities;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
-import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(AnvilScreen.class)
 public class AnvilScreenMixin {
